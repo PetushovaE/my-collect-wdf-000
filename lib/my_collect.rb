b5772ad7db8207f42f62fca8955474a26accd768
Ekaterina Petushova <jevsador@yandex.ru>
2016-07-19 15:52:50 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(languages)
+  empty_array = []
+  counter = 0
+  while counter < languages.length
+    empty_array << yield(languages[counter])
+    counter += 1
+  end
+  empty_array
+end
